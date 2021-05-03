2009bf322f88507679da5eda94bf85472e44508e
hungmch95 <proxvalkryne@gmail.com>
2021-05-03 16:55:42 -0700

    Create 7_ReverseInteger.java

---
+++ Java/7_ReverseInteger.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        
+        int reversed = 0, remainder;
+        
+        while (x != 0){
+            remainder = x%10;
+            x /= 10;
+            if(reversed > Integer.MAX_VALUE/10 || (reversed == Integer.MAX_VALUE/10 & remainder > 7)) return 0;
+            if(reversed < Integer.MIN_VALUE/10 || (reversed == Integer.MIN_VALUE/10 & remainder < -8)) return 0;
+            reversed = reversed*10 + remainder;
+        }
+        return reversed;
+    }
+}
